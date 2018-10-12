8f262000da964be45b3d57f50127a905b15cd381
Anjali <ambhatt@stanford.edu>
2018-10-11 17:38:09 -0700

    fixed bug in full sims code

--- src/culturalcarriers_fixedinitial.R
+++ src/culturalcarriers_fixedinitial.R
@@ -7,6 +7,7 @@ Use source() to run code on server
 Originally written March 2017
 Recoded Apr 2018
 Recoded for fixed initial conditions Jun 2018
+Recoded for bug fixes Oct 2018
 
 @author: Anjali Bhatt
 "
@@ -18,13 +19,13 @@ library(parallel)
 library(ggplot2)
 
 ### Set global parameters for simulations
-n_reps <- 5 # number of replications per set of parameters, per initial condition
+n_reps <- 3 # number of replications per set of parameters, per initial condition
 f <- 30 # number of firms
 n <- 30 # number of employees per firm [10, 100, 1000]
 t <- 120 # number of time periods (months)
 
 ### Read in initializations
-init_conds <- read.csv("data/initial_conditions.csv", header=T)
+init_conds <- read.csv("data/initial_conditions_201810.csv", header=T)
 init_conds <- data.table(init_conds)
 
 ### Make data frame of varying parameter settings
@@ -34,25 +35,24 @@ params <- CJ(
   
   # socialization params (no noise)
   #b0 = 0.0, # asymptotic socialization susceptibility
-  b1 = c(0.0, 0.2, 0.6), # initial socialization susceptibility
+  b1 = c(0.0, 0.3, 0.6), # initial socialization susceptibility
   b2 = 0.30, # speed of socialization susceptibility decline by tenure
   b3 = 0.10, # speed of socialization susceptibility decline by employments
   
   # turnover params
-  alienate = c(0,1), # alienation on
-  r0 = c(0.01, 0.03, 0.05), # turnover base rate (3.5% monthly according to JOLTS)
+  r0 = c(0.0, 0.03, 0.06), # turnover base rate (3.5% monthly according to JOLTS)
   r1 = c(0.1, 1, 10), # turnover alienation rate
-  r2 = 0.06, # max turnover probability
+  r2 = c(0.0, 0.03), # max increase in turnover probability
   
   # hiring params (no noise)
   select = c(0,1), # selectivity on
-  s0 = c(0.1, 1, 10), # hiring selectivity threshold
-  s1 = c(0.01, 0.03, 0.05, 1) # base rate of random entry
+  s0 = c(0.0, 0.03, 0.06, 1), # base rate of random entry
+  s1 = c(0.1, 1, 10) # hiring selectivity threshold
 )
 
 # deduplicate for iterations with functions off
-params[alienate == 0, c('r1','r2') := NA]
-params[select == 0, c('s0') := 10]
+params[r2 == 0, c('r1') := 1]
+params[select == 0, c('s1') := 1] # same as initial var_btwn
 params <- unique(params)
 
 ### Define function for cultural evolution in population
@@ -71,34 +71,34 @@ culture_fn <- function(par) {
     
     sims2 <- data.table(sims)
     sims2[, tenure := tenure + 1]
-    med_cult <- sims[, median(culture, na.rm=T), by=firm]$V1
+    med_cult <- setorder(sims[, median(culture, na.rm=T), by=firm], firm)$V1
     
     ### Turnover first
     # If no alienation, then assume only base turnover rate
-    if (par$alienate==0) {
-      sims2[, firm := 0 + firm * (runif(n*f) > par$r0)]
-    } else {
-      # Otherwise, retention is modeled as a gaussian shape
-      sims2[, firm := 0 + firm * (runif(n*f) > (par$r2 - (par$r2-par$r0) * (par$r1) *
-                                                  sqrt(2*pi) * (dnorm(culture, med_cult[firm], par$r1))))]
-    }
+    # Otherwise, retention is modeled as a gaussian shape
+    sims2[, firm := firm * (runif(n*f) > ((par$r0 + par$r2) - par$r2 * par$r1 *
+                                            sqrt(2*pi) * (dnorm(culture, med_cult[firm], par$r1))))]
+
     #  Restart tenure clock for departed employees
-    sims2[, tenure := 0 + tenure*(firm!=0)]
+    sims2[, tenure := tenure*(firm!=0)]
     
     ### Then socialization
     # no noise, no asymptotic socialization
     sims2[, culture := culture +
             (firm!=0) * (med_cult[firm] - culture) *
-            (par$b1 * exp(-(par$b2 * (tenure-1))-(par$b3 * (employments-1))))]
+            (par$b1 * exp(- (par$b2 * (tenure-1)) - (par$b3 * (employments-1))))]
     
     ### Then hiring
     # Track how many hires made each period
     stats$hires[i+1] <- sum(sims2$firm==0)
     
     if (stats$hires[i+1] > 0) {
-      # Set random order of hiring and prepopulate random entry draw
+      # Set random order of hiring and prepopulate random entrant info
       queue <- data.table(firm = rep(0, stats$hires[i+1]),
-                          draw = runif(stats$hires[i+1]))
+                          culture = 0,
+                          tenure = 0,
+                          employments=ceiling(rlnorm(stats$hires[i+1],0,0.5)),
+                          draw = (runif(stats$hires[i+1]) <= par$s0))
       j <- 1
       for (k in 1:f) {
         hires <- n - sum(sims2$firm==k)
@@ -115,20 +115,21 @@ culture_fn <- function(par) {
         unemployed <- which(sims2$firm==0 & sims$firm!=focal_firm)
         
         # First check for random entry
-        if (queue$draw[h] <= par$s1 | length(unemployed)==0) {
-          sims2 <- rbind(sims2, list(focal_firm, rnorm(1, med_cult[focal_firm], par$s0),
-                                     0, ceiling(rlnorm(1,0,0.5))))
-        } else if (par$select==0) {
+        if (queue$draw[h] | length(unemployed)==0) {
+          queue[h, culture := rnorm(1, med_cult[focal_firm], par$s1)]
+          
           # If no selectivity, then draw random unemployed
+        } else if (par$select==0) {
           chosen <- unemployed[sample.int(length(unemployed), 1)]
           sims2[chosen, `:=`(firm = focal_firm,
                              employments = employments + 1)]
           stats[i+1, rehires := rehires + 1]
-        } else if ((min(abs(sims2$culture[unemployed] - med_cult[focal_firm]))) >= 2*par$s0) {
+          
           # Hire the now-unemployed person with the best cultural fit within the threshold
           # Otherwise hire outside the existing pool
-          sims2 <- rbind(sims2, list(focal_firm, rnorm(1, med_cult[focal_firm], par$s0),
-                                     0, ceiling(rlnorm(1,0,0.5))))
+        } else if ((min(abs(sims2$culture[unemployed] - med_cult[focal_firm]))) >= 2*par$s1) {
+          queue[h, culture := rnorm(1, med_cult[focal_firm], par$s1)]
+          
         } else {
           chosen <- unemployed[which.min(abs(sims2$culture[unemployed] - med_cult[focal_firm]))]
           sims2[chosen, `:=`(firm = focal_firm,
@@ -136,10 +137,12 @@ culture_fn <- function(par) {
           stats[i+1, rehires := rehires + 1]
         }
       }
+      
+      # Append all random entrants and remove all non-hires
+      sims <- rbind(sims2[firm!=0],
+                    queue[culture!=0, list(firm, culture, tenure, employments)])
+      
     }
-    
-    # Remove all non-hires
-    sims <- sims2[firm!=0]
     
     # Calculate statistics
     stats[i+1, var_win := mean(sims[, sd(culture, na.rm=T), by=firm]$V1)]
